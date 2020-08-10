5d94ffc45d3df2852d1b696507a2f109371f9a2d
ayadownsmash <33762053+ayadownsmash@users.noreply.github.com>
2020-08-10 19:23:29 +0600

    Aibyn

--- App.js
+++ App.js
@@ -1,21 +1,50 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { ImageBackground, StyleSheet, Text, View,Image } from "react-native";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+
+const App = () => (
+  <View style={styles.container}>
+    <ImageBackground source={require("./assets/bg.jpg")} style={styles.image}>
+      <Image
+          style={styles.inside}
+          source={require("./assets/logo-grey.png")}
+      />
+      <Image
+          style={styles.logo}
+          source={require("./assets/logo.png")}
+      />
+    </ImageBackground>
+  </View>
+);
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flexDirection: "column",
   },
+  image: {
+    flex: 1,
+    resizeMode: "contain",
+    justifyContent: "center",
+    position: "relative",
+    alignItems: "center"
+  },
+  inside: {
+    height: "60%",
+    width: "60%",
+    justifyContent: "center",
+    alignItems: "center",
+    resizeMode: "contain"
+  
+  },
+  logo: {
+    height: "60%",
+    width: "60%",
+    justifyContent: "center",
+    alignItems: "center",
+    resizeMode: "contain",
+    position: "absolute",
+  }
 });
+
+export default App;
